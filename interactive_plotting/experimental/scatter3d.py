21035996ac058af5645692fb119b984e2cc87a56
Michal Klein <michal.klein@protonmail.com>
2019-12-15 03:53:08 +0100

    Update docstring, remove unused imports

--- interactive_plotting/experimental/scatter3d.py
+++ interactive_plotting/experimental/scatter3d.py
@@ -15,13 +15,15 @@ from bokeh.layouts import row
 from bokeh.plotting import figure
 from bokeh.colors import RGB
 from pandas.api.types import is_categorical_dtype
+from anndata import AnnData
+from typing import Union, Optional, Sequence, Tuple
 
 import matplotlib
 import matplotlib.cm as cm
 import numpy as np
 
 
-DEFAULTS = {
+_DEFAULT = {
     'width': '600px',
     'height': '600px',
     'style': 'dot-color',
@@ -48,7 +50,7 @@ class Surface3d(LayoutDOM):
     z = String
     color = String
 
-    options = Dict(String, Any, default=DEFAULTS)
+    options = Dict(String, Any, default=_DEFAULT)
 
 
 def _to_hex_colors(values, cmap, perc=None):
@@ -65,16 +67,65 @@ def _mpl_to_hex_palette(cmap):
     return [RGB(*tuple(rgb)).to_hex() for rgb in rgb_cmap]
 
 
-def scatter3d(adata, key, basis, components=[0, 1, 2],
-              steps=100,
-              perc=None, n_ticks=10,
-              vertical_ratio=1,
-              show_axes=False,
-              keep_aspect_ratio=True,
-              perspective=True,
-              tooltips=[], cmap=None,
-              dot_size_ratio=0.01,
-              height=1400, width=1400):
+def scatter3d(adata: AnnData,
+              key: str,
+              basis: str = 'umap',
+              components: Sequence[int] = [0, 1, 2],
+              steps: Union[Tuple[int, int], int] = 100,
+              perc: Optional[Tuple[int, int]] = None,
+              n_ticks: int = 10,
+              vertical_ratio: float = 1,
+              show_axes: bool = False,
+              keep_aspect_ratio: bool = True,
+              perspective:bool = True,
+              tooltips: Optional[Sequence[str]] = [],
+              cmap: Optional[matplotlib.colors.ListedColormap] = None,
+              dot_size_ratio: float = 0.01,
+              plot_height: Optional[int] = 1400,
+              plot_width: Optional[int] = 1400):
+    """
+    Parameters
+    ----------
+    adata : :class:`anndata.AnnData`
+        Annotated data object.
+    key
+        Key in `adata.obs` or `adata.var_names` to color in.
+    basis
+        Basis to use.
+    components
+        Components of the basis to plot.
+    steps
+        Step size when the subsampling the data.
+        Larger step size corresponds to higher density of points.
+    perc
+        Percentile by which to clip colors.
+    n_ticks
+        Number of ticks for colorbaar if `key` is not categorical.
+    vertical_ratio
+        Ratio by which to squish the z-axis.
+    show_axes
+        Whether to show axes.
+    keep_aspect_ratio
+        Whether to keep aspect ratio.
+    perspective
+        Whether to keep the perspective.
+    tooltips
+        Keys in `adata.obs` to visualize when hovering.
+    cmap
+        Colormap to use.
+    dot_size_ratio
+        Ratio of dots with respect to the plot size.
+    plot_height
+        Height of the plot in pixels. If `None`, try getting the screen height.
+    plot_width
+        Width of the plot in pixels. If `None`, try getting the screen width.
+
+    Returns
+    -------
+    None
+        Nothing, just plots.
+    """
+
     def _wrap_as_div(row, sep=':'):
         res = []
         for kind, val in zip(tooltips, row):
@@ -86,16 +137,18 @@ def scatter3d(adata, key, basis, components=[0, 1, 2],
         return ''.join(res)
 
     basis_key = f'X_{basis}'
-    assert basis_key in adata.obsm
+    assert basis_key in adata.obsm, f'Basis `{basis_key}` not found in `adata.obsm`.'
     if perc is not None:
-        assert len(perc) == 2
-    assert len(components) == 3
-    assert all(c >= 0 for c in components)
-    assert max(components) < adata.obsm[basis_key].shape[-1]
-    assert key in adata.obs or key in adata.var_names
+        assert len(perc) == 2, f'Percentile must be of length `2`, found `{len(perc)}`.'
+    assert len(components) == 3, f'Number of components must be `3`, found `{len(components)}`.'
+    assert all(c >= 0 for c in components), f'All components must be non-negative, found `{min(components)}`.'
+    assert max(components) < adata.obsm[basis_key].shape[-1], \
+        f'Component `{max(components)}` is >= than number of components `{adata.obsm[basis_key].shape[-1]}`.'
+    assert key in adata.obs or key in adata.var_names, f'Key `{key}` not found in `adata.obs` or `adata.var_names`.'
 
     if steps is not None:
         adata, _ = sample_unif(adata, steps, bs=basis, components=components[:2])
+
     data = dict(x=adata.obsm[basis_key][:, components[0]],
                 y=adata.obsm[basis_key][:, components[1]],
                 z=adata.obsm[basis_key][:, components[2]])
@@ -134,19 +187,23 @@ def scatter3d(adata, key, basis, components=[0, 1, 2],
         data['tooltip'] = adata.obs[tooltips].apply(_wrap_as_div, axis=1)
 
     source = ColumnDataSource(data=data)
-    if width is None or height is None:
+    if plot_width is None or plot_height is None:
         try:
             import screeninfo
             for monitor in screeninfo.get_monitors():
                 break
-            width = max(monitor.width - 300, 0) if width is None else width
-            height = max(monitor.height, 300) if height is None else height
+            plot_width = max(monitor.width - 300, 0) if plot_width is None else plot_width
+            plot_height = max(monitor.height, 300) if plot_height is None else plot_height
+        except ImportError:
+            print('Unable to get the screen size, please install package `screeninfo` as `pip install screeninfo`.')
+            plot_width = 1200 if plot_width is None else plot_width
+            plot_height = 1200 if plot_height is None else plot_height
         except:
-            width = 1200 if width is None else width
-            height = 1200 if height is None else height
+            plot_width = 1200 if plot_width is None else plot_width
+            plot_height = 1200 if plot_height is None else plot_height
 
     surface = Surface3d(x="x", y="y", z="z", color="color",
-                        data_source=source, options={**DEFAULTS,
+                        data_source=source, options={**_DEFAULT,
                                                      **dict(dotSizeRatio=dot_size_ratio,
                                                             showXAxis=show_axes,
                                                             showYAxis=show_axes,
@@ -155,8 +212,8 @@ def scatter3d(adata, key, basis, components=[0, 1, 2],
                                                             yLabel=f'{basis}_{components[1]}',
                                                             zLabel=f'{basis}_{components[2]}',
                                                             showPerspective=perspective,
-                                                            width=f'{width}px',
-                                                            height=f'{height}px',
+                                                            height=f'{plot_height}px',
+                                                            width=f'{plot_width}px',
                                                             verticalRatio=vertical_ratio,
                                                             keepAspectRatio=keep_aspect_ratio,
                                                             showLegend=False,
@@ -168,6 +225,8 @@ def scatter3d(adata, key, basis, components=[0, 1, 2],
     fig.add_layout(to_add, 'left')
 
     # dirty little trick, makes plot disappear
+    # ideally, one would modify the DOM in the .ts file
+    # but I'm just lazy
     fig.xgrid.visible = False
     fig.ygrid.visible = False
     fig.xaxis.visible = False
