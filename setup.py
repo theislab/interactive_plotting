58b127e3e386496ef056cc3ce828499ee8f8ccc0
michalk8 <michal.klein@protonmail.com>
2020-07-19 23:56:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 import os
 
@@ -8,7 +8,9 @@ setup(
     description='Interactive plotting functions for scanpy',
     url='https://github.com/theislab/interactive_plotting',
     license='MIT',
-    packages=['interactive_plotting'],
+    packages=find_packages(),
+    setup_requires=['setuptools_scm'],
+    include_package_data=True,
     install_requires=list(map(str.strip,
                               open(os.path.abspath('requirements.txt'), 'r').read().split())),
 
